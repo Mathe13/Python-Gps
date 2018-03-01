22b2022b24ff81d7edf9cbfc9ecc0ac6160860ab
Mathe13 <matheuslimaaa13@gmail.com>
2018-03-01 11:11:04 -0300

    first commit

---
+++ core.py
@@ -0,0 +1,71 @@
+"""Classes do projeto gps."""
+import requests
+import json
+from unidecode import unidecode as unidecode
+
+# import pprint
+# import string
+
+
+class Gps():
+    """classe com as funçoes principais."""
+
+    def __init__(self, user_key):
+        """Inicia a classe com a chave do usuario."""
+        self.key = user_key
+
+    @staticmethod
+    def normal(palavra):
+        """Normaliza o texto."""
+        return unidecode(palavra)
+
+    def get_json(self, origin, destination):
+        """Pega o json com os dados."""
+        origin = self.normal(origin)
+        destination = self.normal(destination)
+        url = ('https://maps.googleapis.com/maps/api/directions/json?origin=' +
+               origin + '&destination=' + destination + '&key=' + self.key)
+
+        raw_json = requests.get(url)
+        if raw_json:
+            self.decode_json(raw_json.content)
+            return True
+        else:
+            return False
+
+    def decode_json(self, raw_json):
+        """Decodifica o json."""
+        data = json.loads(raw_json)
+        self.caminhos = []
+        for rota in data['routes']:
+            caminho = Caminho(rota)
+            self.caminhos.append(caminho)
+
+
+class Caminho():
+    """classe com dados e metodos referentes ao caminho."""
+
+    def __init__(self, rota):
+        """Inicializador da classe."""
+        self.passos = []
+        for step in rota['legs'][0]['steps']:
+            passo = Passo(step)
+            self.passos.append(passo)
+
+
+class Passo():
+    """classe com dados e metodos referentes ao passo."""
+
+    def __init__(self, step):
+        """Inicializador da classe."""
+        self.distance = step['distance']['text']
+        self.duration = step['duration']['text']
+        self.instruction = self.remove_b_tag(str(step['html_instructions']))
+
+    @staticmethod
+    def remove_b_tag(html_instructions):
+        """Remove as tags."""
+        instruction = html_instructions.replace('<b>', '')
+        instruction = instruction.replace('</b>', '')
+        print('removido:' + instruction)
+        return (instruction)
---
+++ gps.py
@@ -0,0 +1,15 @@
+"""Tentativa de criar um gps."""
+from core import Gps
+key = key = 'AIzaSyC4j5Zm4InJYzTXNytIVgTD7471_CnVpT8'
+
+if __name__ == '__main__':
+    gps = Gps(key)
+    # teste q printa a distancia de cada um dos passos
+    gps.get_json('guaiba cenair maica 110', 'guaiba honorio lemos 181')
+    for caminho in gps.caminhos:
+        for passo in caminho.passos:
+            print('daqui à:' + passo.distance + " " + passo.instruction)
+    # teste = 'Head <b>north</b> on <b>R. Cenair Maicá</b> toward <b>R. Pedro Raymundo</b>'
+    # teste = teste.replace('<b>', '')
+    # teste = teste.replace('</b>', '')
+    # print(teste)
